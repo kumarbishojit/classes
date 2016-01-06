10d117926be35e4f5370259fceae966bdf04c557
Kumar Bishojit Paul <info@bikiran.com>
2016-01-06 13:35:10 +0600

    Update query.php

--- query.php
+++ query.php
@@ -1,16 +1,17 @@
 <?php
 class query{
 	function str($str){
-		return mysql_query($str);
+		global $l;
+		return mysql_query($l=$str);
 	}
 	function byKey($query_str, $key=""){
-		global $msg;
+		global $msg, $l;
 		
 		if($key=="")
 		$key="auto";
 		
 		if(!$query=mysql_query($l=$query_str)){
-			$msg=mysql_error()." ($l) on query->byKey";
+			$msg=mysql_error()." on query->byKey";
 			return false;
 		}
 		while($row_ar=mysql_fetch_assoc($query)){
@@ -33,55 +34,9 @@ class query{
 		$all_ar=$this->byKey($query_str);
 		return $this->array2set($all_ar, $key);
 	}
-	function byTableKeySl($table, $key, $sl=false){
-		global $msg;
-		if($sl=="first")
-		$query_str="SELECT `$key` FROM `$table` ORDER BY `$table`.`sl` ASC LIMIT 1";
-		else if($sl=="last")
-		$query_str="SELECT `$key` FROM `$table` ORDER BY `$table`.`sl` DESC LIMIT 1";
-		else if($sl=="min")
-		$query_str="SELECT `$key` FROM `$table` ORDER BY `$table`.`$key` ASC LIMIT 1";
-		else if($sl=="max")
-		$query_str="SELECT `$key` FROM `$table` ORDER BY `$table`.`$key` DESC LIMIT 1";
-		else if($sl=="rand")
-		$query_str="SELECT `$key` FROM `$table` ORDER BY  RAND() LIMIT 1";
-		else{
-			$msg="'sl' Not Defined on query->byTableKeySl";
-			return false;
-		}
-		
-		if(!$query=mysql_query($query_str)){
-			$msg=mysql_error()." on query->byTableKeySl";
-			return false;
-		}
-		
-		while($row_ar=mysql_fetch_assoc($query)){
-			$msg="OK";
-			return $row_ar[$key];
-		}
-	}
-	function arrayToSql($table, $parameters, $conditions=1, $orderBy="", $limit="", $key="auto"){
-		global $msg;
-		
-		if(!$table){
-			$msg="Table Not Defined on query->arrayToSql";
-			return false;
-		}
-		else if(!$parameters){
-			$msg="Parameters Not Defined on query->arrayToSql";
-			return false;
-		}
-		else if(!$out_all_ar=$this->byKey($l="SELECT $parameters FROM `$table` WHERE $conditions ".($orderBy?"ORDER BY $orderBy":"").($limit?"LIMIT $limit":""), $key)){
-			$msg="SQL Error ($l) on query->arrayToSql";
-			return false;
-		}
-		else{
-			$msg="OK";
-			return $out_all_ar;
-		}
-	}
 	function update($table, $post_ar, $sl, $history=true){
-		global $msg;
+		global $msg, $l;
+		
 		if(!$table){
 			$msg="No Table Name on query->update";
 			return false;
@@ -90,19 +45,22 @@ class query{
 			$msg="Post Error on query->update";
 			return false;
 		}
+		else if(!$row_info_ar=$this->byKey("SELECT * FROM `$table` WHERE `sl`='$sl'")){
+			$msg="Nothing To Update on query->update";
+			return false;
+		}
 		else{
-			$row_info_ar=$this->byKey("SELECT * FROM `$table` WHERE `sl`='$sl'", "auto");
 			//--Creating Queryes
 			foreach($post_ar as $key => $val){
 				$q1_ar[]="`$key`='".mysql_real_escape_string($val)."'";
 				
 				if($history==true){
 					if($val!=$row_info_ar[0][$key])
-					$hist_ar[$key]=mysql_real_escape_string($row_info_ar[0][$key]);
+					$hist_ar[$key]=$row_info_ar[0][$key];
 				}
 			}
 			//--Query Action
-			if(mysql_query($l="UPDATE `$table` SET ".implode(", ", $q1_ar)." WHERE `sl`='$sl';")){
+			if(mysql_query($l2="UPDATE `$table` SET ".implode(", ", $q1_ar)." WHERE `sl`='$sl';")){
 				if($history==true && $hist_ar){
 					foreach($hist_ar as $col => $oldVal){
 						$values_all_ar[$col]['tbl']=$table;
@@ -110,25 +68,27 @@ class query{
 						$values_all_ar[$col]['tsl']=$sl;
 						$values_all_ar[$col]['oldVal']=$oldVal;
 					}
-					$this->insert_all("history", $values_all_ar);
+					$this->insertAll("history", $values_all_ar);
 				}
+				$l=$l2;
 				$msg="OK";
 				return true;
 			}
 			else{
-				$msg="MySql Query ($l) Error on query->update";
+				$msg="MySql Query Error on query->update";
 				return false;
 			}
 		}
 	}
-	function insert_one($table, $post_ar){
-		global $msg;
+	function insertOne($table, $post_ar){
+		global $msg, $l;
+		
 		if(!$table){
-			$msg="No Table Name on query->insert_one";
+			$msg="No Table Name on query->insertOne";
 			return false;
 		}
 		else if(!is_array($post_ar)){
-			$msg="Post Error on query->insert_one";
+			$msg="Post Error on query->insertOne";
 			return false;
 		}
 		else{
@@ -148,20 +108,20 @@ class query{
 				return true;
 			}
 			else{
-				$msg="MySql Query ($l) Error on query->insert_one";
+				$msg="MySql Query Error on query->insertOne";
 				return false;
 			}
 		}
 	}
-	function insert_all($table, $post_all_ar){
-		global $msg;
+	function insertAll($table, $post_all_ar){
+		global $msg, $l;
 		
 		if(!$table){
-			$msg="No Table Name on query->insert_all";
+			$msg="No Table Name on query->insertAll";
 			return false;
 		}
 		else if(!is_array($post_all_ar)){
-			$msg="Post Error on query->insert_all";
+			$msg="Post Error on query->insertAll";
 			return false;
 		}
 		else{
@@ -188,55 +148,10 @@ class query{
 				return true;
 			}
 			else{
-				$msg="MySql Query ($l) Error on query->insert_one";
+				$msg="MySql Query Error on query->insertAll";
 				return false;
 			}
 		}
-	}
-	function delete($table, $conditions){
-		global $msg;
-		if(!$table){
-			$msg="No Table Name on query->delete";
-			return false;
-		}
-		else if(!$conditions){
-			$msg="Condition Error on query->delete";
-			return false;
-		}
-		else if(!mysql_query("DELETE FROM `$table` WHERE $conditions ")){
-			$msg="MySql Query Error on query->delete";
-			return false;
-		}
-		else
-		return true;
-	}
-	function row($tbl, $col, $val, $condition="="){
-		$out_ar=$this->byKey($l="SELECT * FROM `$tbl` WHERE `$col` $condition '$val'");
-		if(!$out_ar){
-			$msg="MySql Query Error (".mysql_error().") on query->row";
-			return false;
-		}
-		else{
-			$msg="OK";
-			return $out_ar;
-		}
-	}
-	function attrTblTsl_ar($tbl, $tsl_ar, $key_ar=array()){
-		if(!is_array($tsl_ar)){
-			$out_st=$tsl_ar;
-			$tsl_ar=array($tsl_ar);
-		}
-		
-		//--Collect TRSL Attr
-		if($attr_all_ar=$this->byKey("SELECT `tsl`, `key_word`, `val` FROM `detinfo` WHERE `tbl`='$tbl' AND (`tsl`='".implode("' OR `tsl`='", $tsl_ar)."') AND `st`='a'", "auto"))
-		foreach($attr_all_ar as $det_ar){
-			$out_all_ar[$det_ar['tsl']][$det_ar['key_word']]=$det_ar['val'];
-		}
-		
-		if($out_st)
-		return $out_all_ar[$out_st];
-		else
-		return $out_all_ar;
 	}
 	function orStr($ar, $key, $emptyReturns=0){
 		if(!$ar)
@@ -250,10 +165,6 @@ class query{
 		else
 		return "(`$key`!='".implode("' AND `$key`!='", $ar)."')";
 	}
-	
-	/*function orStrInv($ar, $key){
-		return "(`$key`!='".implode("' AND `$key`!='", $ar)."')";
-	}*/
 	function array2set($array2d, $key){
 		global $msg;
 		if(!is_array($array2d)){
@@ -270,22 +181,22 @@ class query{
 		}
 		return $out_ar;
 	}
-	function mysql($queryTxt){
-		global $msg;
-		if(!$queryTxt){
-			$msg="Invalide \$queryTxt on \$query->mysql";
+	function recentEntry($tbl){
+		global $msg, $l;
+		$tbl_ar=explode("`.`", $tbl);
+		$row_info_ar=$this->byKey($l="SELECT * FROM `$tbl` WHERE `ip`='".USER_IP."' AND `time`='".TIME."' AND `oprtr`='$_SESSION[userSl]' ORDER BY `".($tbl_ar[1]?$tbl_ar[1]:$tbl_ar[0])."`.`sl` DESC LIMIT 1");
+		if(mysql_error()==true){
+			$msg="mySql Error on \$query->recentEntry";
 			return false;
 		}
-		
-		foreach($array2d as $ar){
-			$out_ar[$ar[$key]]=$ar[$key];
+		else if(!$row_info_ar){
+			$msg="No Row Found on \$query->recentEntry";
+			return false;
 		}
-		return $out_ar;
-	}
-	function recentEntry($tbl){
-		$tbl_ar=explode("`.`", $tbl);
-		$row_info_ar=$this->byKey("SELECT * FROM `$tbl` WHERE `ip`='".USER_IP."' AND `time`='".TIME."' AND `oprtr`='$_SESSION[userSl]' ORDER BY `".($tbl_ar[1]?$tbl_ar[1]:$tbl_ar[0])."`.`sl` DESC LIMIT 1");
-		return $row_info_ar[0];
+		else{
+			$msg="OK";
+			return $row_info_ar[0];
+		}
 	}
 	function saveLog($tbl, $message){
 		$in_ar['tbl']=$tbl;
@@ -294,18 +205,34 @@ class query{
 		$in_ar['oldVal']=$message;
 		$this->insertOne(DB_USER."`.`history", $in_ar);
 	}
-	function listDropDown($type){
-		global $global_listing_all_ar;
+	function listAllGlobal($type_hash){  //  #education#abc#def
+		global $global_listing_all_ar, $userInfo_ar, $l;
+		unset($global_listing_all_ar);
+		
+		$row_all_ar=$this->byKey($l="SELECT * FROM `".DB_USER."`.`list` WHERE (`orgSl`='10000000' OR `orgSl`='$userInfo_ar[orgSl]') AND (`type`='".str_replace("#", "' OR `type`='", $type_hash)."') AND `st`='act'");
+		
+		if($row_all_ar)
+		foreach($row_all_ar as $det_ar){
+			$global_listing_all_ar[$det_ar['type']][]=$det_ar;
+		}
+		
+		if($row_all_ar)
+		return true;
+		
+		return false;
+	}
+	function listAr($type){
+		global $global_listing_all_ar, $userInfo_ar, $l;
 		
 		if(!$global_listing_all_ar[$type])
-		$global_listing_all_ar[$type]=$this->byKey("SELECT * FROM `".DB_USER."`.`list` WHERE (`orgSl`='10000000' OR `orgSl`='$userInfo_ar[orgSl]') AND `type`='$type' AND `st`='act'");
+		$global_listing_all_ar[$type]=$this->byKey($l="SELECT * FROM `".DB_USER."`.`list` WHERE (`orgSl`='10000000' OR `orgSl`='$userInfo_ar[orgSl]') AND `type`='$type' AND `st`='act'");
 		return $global_listing_all_ar[$type];
 	}
 	function listToFull($type, $key, $col='val'){
-		global $global_listing_all_ar;
+		global $global_listing_all_ar, $userInfo_ar, $l;
 		
 		if(!$global_listing_all_ar[$type])
-		$global_listing_all_ar[$type]=$this->byKey("SELECT * FROM `".DB_USER."`.`list` WHERE (`orgSl`='10000000' OR `orgSl`='$userInfo_ar[orgSl]') AND `type`='$type' AND `st`='act'", "keyTxt");
+		$global_listing_all_ar[$type]=$this->byKey($l="SELECT * FROM `".DB_USER."`.`list` WHERE (`orgSl`='10000000' OR `orgSl`='$userInfo_ar[orgSl]') AND `type`='$type' AND `st`='act'", "keyTxt");
 		
 		$global_listing_all_ar[$type]['$key'][$col];
 	}
