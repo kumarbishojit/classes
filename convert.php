cae813c137de449d3fcd003c975f85d416d768d0
Kumar Bishojit Paul <info@bikiran.com>
2016-01-24 12:18:19 +0600

    Update convert.php

--- convert.php
+++ convert.php
@@ -254,6 +254,8 @@ class convert{
         }
 	function cleanStr($string) {
 	   $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+	   $string = str_replace('--', '-', $string); // Replaces all spaces with hyphens.
+	   $string = str_replace('--', '-', $string); // Replaces all spaces with hyphens.
 	
 	   return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
 	}
