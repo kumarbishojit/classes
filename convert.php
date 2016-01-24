52e073641c986a4c17a73655b5032e9453a4476c
Kumar Bishojit Paul <info@bikiran.com>
2016-01-24 12:13:31 +0600

    Update convert.php

--- convert.php
+++ convert.php
@@ -252,6 +252,11 @@ class convert{
                 }
                 return $token;
         }
+	function cleanStr($string) {
+	   $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+	
+	   return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+	}
 }
 $convert		= new convert; 
 ?>
