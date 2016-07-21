1f0901580ba1470e7eeffecd2341041996fdfc0f
Kumar Bishojit Paul <info@bikiran.com>
2016-07-22 00:31:38 +0600

    Update bik-4-0-0.js

--- bik-4-0-0.js
+++ bik-4-0-0.js
@@ -265,6 +265,26 @@ function liveSearch(searchId, jsonLink){
 	});
 };
 
+function setCookie(cname, cvalue, exsec) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exsec*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname+"="+cvalue+"; expires="+expires+"; path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
 //--Tools Function CK Editor
 function ckEditorFull(id){
 	CKEDITOR.plugins.addExternal( 'onchange', 'onchange/' );
